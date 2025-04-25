2578fe1a5e88d3977d3e696d3f13a044fb229c01
map_gisworld <map.gisworld@gmail.com>
2025-04-25 23:14:35 +0200

    Completata pagina index, completata pagina fair play

--- script_python.py
+++ script_python.py
@@ -6,4 +6,6 @@ statistiche_fantacalcio=pd.read_excel("DATI/DATI FANTACALCIO.xlsx", sheet_name="
 # il file contiene le statistiche dei calciatori delle squadre fanta ecr
 file_merge=squadre_fantaecr.merge(statistiche_fantacalcio,how="inner",on="Calciatore")
 
+# Questo file andrà ad aggiornare il foglio Classifica Fair Play presente sul file excel DATI FANTACALCIO
+# In questa fase i dati del Fair Play sono stati aggiornati con le squadre successive al calciomercato, bisogna aggiungere i voti dei giocatori svincolati
 file_merge.to_excel("DATI/STATISTICHE_FANTAECR_BASE.xlsx", sheet_name="STATISTICHE_FANTAECR", index=False)
